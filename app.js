a20496c5d6739058946635e8392249ba99a139bb
nadhilkn <nadhunadhil33429@gmail.com>
2025-07-18 12:31:37 +0530

    18-07 starting

---
+++ app.js
@@ -0,0 +1,8 @@
+import express from'express'
+import mongoose from 'mongoose'
+import adminRouter from './routers/adminRouter.js'
+const uri = "mongodb://127.0.0.1:27017/ecombackend"
+mongoose.connect(uri).then(()=>{
+    console.log("database connected")
+})
+const app = express();
---
+++ routers/adminRouter.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import user from '../models/user.js'
+const router = express.Router()
+router.get()
+
+export default router
