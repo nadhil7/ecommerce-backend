918b739d448cc6832a9fc97e4336f84575d4b378
nadhilkn <nadhunadhil33429@gmail.com>
2025-08-09 14:18:51 +0530

    4th

--- middleware/Error.js
+++
@@ -1,6 +0,0 @@
-import express from 'express'
-const app = express()
-app.use((req,res,err,next)=>{
-    
-})
-
--- middleware/adminmiddleware.js
+++ middleware/adminmiddleware.js
@@ -1,8 +1,7 @@
 
-export function middleware(req,res,next){
-    if(!req.session.adminId)
-    {
-        return res.status(404).json({message:"entry restricted!!"})
+export function middleware(req, res, next) {
+    if (!req.session.adminId) {
+        return res.status(404).json({ message: "entry restricted!!" })
     }
     next()
-    }
+}
